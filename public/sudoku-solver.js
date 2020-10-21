2dfdc94b8f26cc8d44374215e6628641788835e8
Menno van Balen <mennovanbalen@gmail.com>
2020-10-21 11:47:48 +0100

    added callback to validate input in textarea and grid

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -1,5 +1,6 @@
 const textArea = document.getElementById("text-input");
 const inputCells = document.querySelectorAll(".sudoku-input");
+const errorDiv = document.getElementById("error-msg");
 // console.log(inputCells);
 
 // import { puzzlesAndSolutions } from './puzzle-strings.js';
@@ -10,9 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
     "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
   textAreaChanged();
 });
-const validValue = /^[0-9]|\./;
+const validValue = /^[0-9.]*$/;
+
+const validateInput = (string) => {
+  errorDiv.innerText = "";
+  if (!validValue.test(string)) {
+    // console.log("false");
+    errorDiv.innerText = "Error: invalid character";
+    return false;
+  }
+};
 
 const textAreaChanged = () => {
+  // errorDiv.innerText = "";
+  // if (!validValue.test(textArea.value)) {
+  //   // console.log("false");
+  //   errorDiv.innerText = "Error: invalid character";
+  //   return;
+  // }
+  if (validateInput(textArea.value) === false) return;
+
   let textAreaValues = textArea.value.split("");
   // console.log(textAreaValues);
   textAreaValues.forEach((val, i) => {
@@ -23,7 +41,7 @@ const textAreaChanged = () => {
     //   inputCells[i].value = ".";
     // }
   });
-  console.log("textAreaChanged called");
+  // console.log("textAreaChanged called");
 };
 
 const inputCellChanged = () => {
@@ -32,7 +50,16 @@ const inputCellChanged = () => {
   inputCells.forEach((cell) => {
     valueString += cell.value;
   });
-  console.log("inputCellChanged called");
+  // console.log("inputCellChanged called");
+
+  // errorDiv.innerText = "";
+  // if (!validValue.test(valueString)) {
+  //   // console.log("false");
+  //   errorDiv.innerText = "Error: invalid character";
+  //   return;
+  // }
+  if (validateInput(valueString) === false) return;
+
   textArea.value = valueString;
 };
 
